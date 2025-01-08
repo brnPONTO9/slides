0643181e286788a9acb16a7021a3dfe6da15ece5
Bruno Silva <brunosilva_2002@outlook.com>
2025-01-08 10:06:20 -0300

    Update effect.js

--- effect.js
+++ effect.js
@@ -1,13 +1,13 @@
 <script>
- 				let currentIndex = 0;
-					const slides = document.querySelector('.slidees');
-					const totalSlides = slides.children.length;
+let currentIndex = 0;
+	const slides = document.querySelector('.slidees');
+	const totalSlides = slides.children.length;
 
-					function showNextSlide() {
-					currentIndex = (currentIndex + 1) % totalSlides;
-					const offset = -currentIndex * 250; 
-					slides.style.transform = `translateX(${offset}px)`;
-					}
+	function showNextSlide() {
+	currentIndex = (currentIndex + 1) % totalSlides;
+	const offset = -currentIndex * 250; 
+	slides.style.transform = `translateX(${offset}px)`;
+	}
 
-					setInterval(showNextSlide, 3000);
-			</script>
+	setInterval(showNextSlide, 3000);
+</script>
